926a8264dab8bc5ecc7ddadbcbe2d0856d02118e
George3d6 <george@cerebralab.com>
2023-06-17 18:34:33 +1200

    better routes

--- main.py
+++ main.py
@@ -1,151 +1,231 @@
+import datetime
 import json
+import sys
 import aiosqlite
 from fastapi import FastAPI, Depends, HTTPException, Request
 import os
 import uvicorn
 from pydantic import BaseModel
 import aiohttp
+from typing import Annotated
 
 
+# Note orm will make it harder to split and modify, though make coding easier, so for now we aren't
+# + async sqlalchemy is ergh and I don't want to investigate async ORMs right now
 async def create_tables():
-    async with aiosqlite.connect("spile.db") as db:
-        await db.execute(
-            """
-            CREATE TABLE IF NOT EXISTS users (
-                email TEXT PRIMARY KEY,
-                auth_token TEXT,
-                is_admin BOOL,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            );
-            """
-        )
-
-        await db.execute(
-            """
-            CREATE TABLE IF NOT EXISTS items (
-                uid TEXT PRIMARY KEY,
-                type TEXT,
-                content TEXT,
-                resonance INTEGER,
-                feedback TEXT,
-                view_date TIMESTAMP,
-                received_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (email) REFERENCES users(email)
-            );
-            """
-        )
-
-        await db.execute(
-            """
-            CREATE TABLE IF NOT EXISTS sources (
-                source TEXT,
-                type TEXT,
-                FOREIGN KEY (email) REFERENCES users(email)
+    async with aiosqlite.connect("spile.db") as con:
+        async with con.cursor() as cur:
+            await cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS users (
+                    email TEXT PRIMARY KEY,
+                    auth_token TEXT,
+                    is_admin BOOL,
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                );
+                """
             )
-            """
-        )
 
-        await db.commit()
+            await cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS items (
+                    uid TEXT PRIMARY KEY,
+                    type TEXT,
+                    content TEXT,
+                    resonance INTEGER,
+                    feedback TEXT,
+                    view_date TIMESTAMP,
+                    received_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    FOREIGN KEY (email) REFERENCES users(email)
+                );
+                """
+            )
+
+            await cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS sources (
+                    source TEXT,
+                    type TEXT,
+                    FOREIGN KEY (email) REFERENCES users(email)
+                )
+                """
+            )
+
+            await con.commit()
+
+
+async def insert(table: str, values: list[dict]):
+    async with aiosqlite.connect("spile.db") as con:
+        async with con.cursor() as cur:
+            question_marks = ",".join(["?"] * len(values[0]))
+            ordered_keys = list(values[0].keys())
+            cols = ",".join(ordered_keys)
+            fmt_values = [
+                [values[i][k] for k in ordered_keys] for i in range(len(values))
+            ]
+            q = f"INSERT INTO {table} ({cols}) VALUES ({question_marks})"
+            cur.execute(q, fmt_values)
+            con.commit()
+
+
+async def select(q: str, one: bool = False):
+    async with aiosqlite.connect("spile.db") as con:
+        con.row_factory = aiosqlite.Row
+        async with con.cursor() as cur:
+            await cur.execute(q)
+            rows = await cur.fetchall()
+            res = [dict(x) for x in rows]
+            if one:
+                if len(rows) > 1:
+                    raise Exception(f"Expected only one row, got: {len(rows)}")
+                if len(rows) < 1:
+                    return None
+                return res[0]
+            return res
+
 
 async def auth(req: Request):
     auth_token = req.headers.get("auth_token", None)
     async with aiosqlite.connect("spile.db") as db:
-        res = await db.execute(f"SELECT email, is_admin FROM users WHERE auth_token='{auth_token}'")
-        
-        if len(res) == 0:
+        res = await select(
+            f"SELECT email, is_admin FROM users WHERE auth_token='{auth_token}'", True
+        )
+
+        if res is None:
             raise HTTPException(status_code=401, detail="Unauthorized")
-        email = res[0][0], res[0][1]
-        return email
+        return res["email"], res["is_admin"]
+
 
 app = FastAPI()
 
 
 async def detect_source_type(source: str):
     if "@" in source and "get_feed" in source:
-        return  "spiel"
+        return "spiel"
     return "rss"
+
 
 async def consume_source(source: str, source_type: str, email: str):
     if source_type == "spiel":
-        all_items = await aiothttp.get(source)
-        async with aiosqlite.connect("spile.db") as db:
-            for item in all_items:
-                uid = item.uid
-                res = await db.execute("SELECT COUNT(*) as c FROM items WHERE email='{email}' and uid='{uid}'")
-                if res[0][0] > 0:
-                    # @TODO If item already exists merge all the `views` into one
-                    pass
-                else:
-                    await db.execute("INSERT INTO items(item_content, item_type, uid, email) VALUES (?, ?, ?, ?)", (item.content, item.type, uid, email))
-            await db.commit()
+        all_items = await aiohttp.get(source)
+        for item in all_items:
+            uid = item.uid
+            res = await select(
+                "SELECT COUNT(*) as c FROM items WHERE email='{email}' and uid='{uid}'",
+                True,
+            )
+            if res["c"] > 0:
+                # @TODO If item already exists merge all the `views` into one
+                pass
+            else:
+                await insert(
+                    "items",
+                    [
+                        {
+                            "item_content": item.content,
+                            "item_type": item.type,
+                            "uid": uid,
+                            "email": email,
+                        }
+                    ],
+                )
     elif source_type == "rss":
         pass
     else:
         raise ValueError(f"Unknown source type: `{source_type}` for source `{source}`!")
 
 
-@app.post("/create_user")
-async def create_user(email: str, is_admin: bool):
-    if is_admin:
+# token: :
+async def create_user(
+    email: str, is_admin: bool, auth_data: Annotated[(str, str), Depends(auth)]
+):
+    if auth_data[1]:
         new_user_auth_token = generate_auth_token()
         async with aiosqlite.connect("spile.db") as db:
-            await db.execute(f"INSERT INTO users VALUES ('{email}', '{new_user_auth_token}', {str(is_admin).lower()})")
+            await db.execute(
+                f"INSERT INTO users VALUES ('{email}', '{new_user_auth_token}', {str(is_admin).lower()})"
+            )
             await db.commit()
-    return {
-        "email": email,
-        "auth_token": new_user_auth_token
-    }
+    return {"email": email, "auth_token": new_user_auth_token}
+
 
 @app.post("/rate_item")
-async def rate_item(uid: str, feedback: str, resonance: int):
+async def rate_item(
+    uid: str,
+    feedback: str,
+    resonance: int,
+    auth_data: Annotated[(str, str), Depends(auth)],
+):
     async with aiosqlite.connect("spile.db") as db:
-        await db.execute(f"UPDATE TABLE items SET feedback='{feedback}', resonance={resonance} WHERE uid='{uid}' AND email='{auth_email}'")
+        await db.execute(
+            f"UPDATE TABLE items SET feedback='{feedback}', resonance={resonance} WHERE uid='{uid}' AND email='{auth_data[0]}'"
+        )
 
-@app.post("/get_items")
-async def get_items():
+
+@app.get("/get_items")
+async def get_items(auth_data: Annotated[(str, str), Depends(auth)]):
     async with aiosqlite.connect("spile.db") as db:
-        all_consumeable = await db.execute(f"SELECT * FROM items WHERE email='{auth_email}'")
-    return all_consumeable
+        all_consumeable = await db.execute(
+            f"SELECT * FROM items WHERE email='{auth_data[0]}'"
+        )
+    return {"updateAt": datetime.datetime.now(), "items": all_consumeable}
+
 
 @app.post("/add_source")
-async def add_source(source: str):
+async def add_source(source: str, auth_data: Annotated[(str, str), Depends(auth)]):
     source_type = await detect_source_type(source)
     async with aiosqlite.connect("spile.db") as db:
-        await db.execute(f"INSERT INTO sources VALUES ('{source}', '{source_type}', '{auth_email}')")
+        await db.execute(
+            f"INSERT INTO sources VALUES ('{source}', '{source_type}', '{auth_data[0]}')"
+        )
         await db.commit()
+
 
 @app.get("/get_feed/{user_email}")
 async def get_feed():
     async with aiosqlite.connect("spile.db") as db:
-        all_consumeable = await db.execute(f"SELECT uid, content, type, resonance, feedback FROM items WHERE email='{user_email}' AND view_date is not null")
+        all_consumeable = await db.execute(
+            f"SELECT uid, content, type, resonance, feedback FROM items WHERE email='{user_email}' AND view_date is not null"
+        )
         consumable_objs = {"updateAt": datetime.datetime.now(), "items": []}
         for val in all_consumeable:
             consumable_obj = json.loads(val[1])
-            consumable_obj.views.append({
-                "viewer": user_email,
-                "resonance": val[3],
-                "feedback": val[4],
-
-            })
-            consumable_objs.append({
-                "uid": val[0],
-                "type": val[2],
-                "content": consumable_obj,
-            })
+            consumable_obj.views.append(
+                {
+                    "viewer": user_email,
+                    "resonance": val[3],
+                    "feedback": val[4],
+                }
+            )
+            consumable_objs.append(
+                {
+                    "uid": val[0],
+                    "type": val[2],
+                    "content": consumable_obj,
+                }
+            )
     return all_consumeable
+
 
 @app.get("/get_feed/{user_email}.rss")
 async def get_feed_rss(user_email: str):
-    return 'Not implemented'
+    return "Not implemented"
+
 
 @app.on_event("startup")
 async def startup():
     pass
 
+
 if __name__ == "__main__":
     import asyncio
+
     asyncio.run(create_tables())
-    if os.environ.get("APP_ENV", "development") == "production":
-        uvicorn.run("api:app", host="0.0.0.0", port=8080, workers=4)
+    if len(sys.argv) > 1:
+        port = sys.argv[1]
     else:
-        uvicorn.run("api:app", host="0.0.0.0", port=8080, workers=1)
+        port = "8080"
+    if os.environ.get("APP_ENV", "development") == "production":
+        uvicorn.run("api:app", port=port, host="0.0.0.0", workers=4)
+    else:
+        uvicorn.run("api:app", port=port, host="0.0.0.0", workers=1)
---
+++ tests.py
@@ -0,0 +1,84 @@
+import requests
+import sqlite3
+import json
+
+
+# A server runs on 8080 using spile.db with and admin account
+base = "http://localhost:8080"
+
+
+# We make the admin account
+db = sqlite3.connect("spile.db")
+db.execute("INSERT INTO users VALUES ('test@test.com', 'abcdefg', true)")
+db.commit()
+
+## And create two users
+user1_email = "user1@test.com"
+user2_email = "user2@test.com"
+user1_auth = requests.post(
+    base + "/create_user",
+    headers={"auth_token": "abcdefg"},
+    data={"email": user1_email},
+).json()["auth_token"]
+user2_auth = requests.post(
+    base + "/create_user",
+    headers={"auth_token": "abcdefg"},
+    data={"email": user2_email},
+).json()["auth_token"]
+
+# Both users should have no items
+for auth_token in [user1_auth, user2_auth]:
+    items = requests.get(
+        base + "/get_items", headers={"auth_token": auth_token}
+    ).json()["items"]
+    assert len(items) == 0
+
+
+# Artificially add items
+for email in [user1_email, user2_email]:
+    with sqlite3.connect("spile.db") as db:
+        db.execute(
+            f""" INSERT INTO items (uid, content, email) VALUES (?, ?, ?)""",
+            (
+                f"uid_for_{email}_content",
+                json.dumps(
+                    {
+                        "title": "A",
+                        "content": "blah blah",
+                        "link": "http://baz.com/rara.txt",
+                    }
+                ),
+                email,
+            ),
+        )
+
+# Now both users should get 1 item
+for auth_token in [user1_auth, user2_auth]:
+    items = requests.get(
+        base + "/get_items", headers={"auth_token": auth_token}
+    ).json()["items"]
+    assert len(items) == 0
+
+
+# Now subscribe them to each other
+
+
+# They should still have 1 item each
+
+
+# user1 reads and rates an item
+
+
+# user2 should have 2x items now
+
+
+# user2 rates his original item
+
+
+# Both now have 1x item, but with the meaningdata from the other on top and the uids reversed
+
+
+# Adding an rss feed to user1 results in him getting more items from that
+
+
+# And user1 is also able to generate a valid rss feed with the same amount of items as the source + 1 (the one he rated already)
